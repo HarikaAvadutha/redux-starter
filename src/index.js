99815f4efc44826c30491406ab012996ac320701
Harika Avadutha <harika.avadutha@GGKU2-MACPRO001.local>
2020-09-22 11:16:03 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import store from './store';
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState());
+});
+
+store.dispatch(bugAdded('Bug 1'));
+console.log(store.getState());
+
+store.dispatch(bugResolved(1));
+
+unsubscribe();
+
+store.dispatch(bugRemoved(1));
+
+console.log(store.getState());
